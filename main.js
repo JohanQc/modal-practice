8dd2e8a37961424ebb6c5d18bbde02b226bb43ed
JohanCode <deimonsu@gmail.com>
2023-01-09 12:11:13 -0500

    chore: add modal funtionality.

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-const openModalBtn = document.querySelector('#open-modal-btn')
-const closeModal = document.querySelector('.modal__close')
+const openModalBtn = document.getElementById('open-modal-btn')
+const modalOverlay = document.getElementById('modal__overlay')
+const btnClose = document.getElementById('btn-Close')
+const modalElement = document.getElementById('modal')
+
+
+openModalBtn.addEventListener('click', function () {
+ modalOverlay.classList.add('modal__overlay--active')
+ modalElement.classList.add('modal--active')
+})
+
+
+btnClose.addEventListener('click', function () {
+ // closeModal.classList.remove('') 
+ modalOverlay.classList.remove('modal__overlay--active')
+ modalElement.classList.remove('modal--active')
+})
