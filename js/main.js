2671cf6d86b563689eddb9c879167fff218a8e9d
JohanCode <deimonsu@gmail.com>
2023-01-18 22:52:52 -0500

    message refactor: improve decode adding module js.

---
+++ js/main.js
@@ -0,0 +1,42 @@
+import { closeModal, openModal, renderPosts, posts } from "./utils.js";
+
+const openModalBtn = document.getElementById("open-modal-btn");
+const btnClose = document.getElementById("btn-Close");
+const btnBack = document.getElementById("btn-Back");
+const btnCancel = document.getElementById("btn-cancel");
+const btnCreate = document.getElementById("btn-create");
+const inputImg = document.getElementById("inputImg");
+const inputTitle = document.getElementById("inputTitle");
+const inputDescription = document.getElementById("inputDescription");
+
+openModalBtn.addEventListener("click", openModal);
+
+btnClose.addEventListener("click", closeModal);
+
+btnBack.addEventListener("click", closeModal);
+
+btnCancel.addEventListener("click", closeModal);
+
+btnCreate.addEventListener("click", function () {
+  const imgValue = inputImg.value
+  const descriptionValue = inputDescription.value
+  const titleValue = inputTitle.value
+  if (
+    [imgValue, descriptionValue, titleValue].filter((item) => item).length !== 3
+  ) {
+    alert("Por favor completa el form");
+    return;
+  }
+
+  posts.push({
+    img: imgValue,
+    title: titleValue,
+    description: descriptionValue,
+    id: new Date().valueOf(),
+  });
+  renderPosts();
+  closeModal();
+  localStorage.setItem("posts", JSON.stringify(posts));
+});
+
+renderPosts();
---
+++ js/utils.js
@@ -0,0 +1,49 @@
+const modalOverlay = document.getElementById("modal__overlay");
+const modalElement = document.getElementById("modal");
+const circle = document.getElementById("container_btn");
+const postsContainerHTML = document.getElementById("posts");
+const storagePosts = localStorage.getItem("posts")
+export const posts = JSON.parse(storagePosts) || [];
+
+export function closeModal() {
+  modalOverlay.classList.remove("modal__overlay--active");
+  modalElement.classList.remove("modal--active");
+  circle.classList.remove("container-btn--active");
+  document.body.style.overflow = "initial";
+}
+
+export function openModal() {
+  modalOverlay.classList.add("modal__overlay--active");
+  modalElement.classList.add("modal--active");
+  circle.classList.add("container-btn--active");
+  document.body.style.overflow = "hidden";
+}
+
+export function renderPosts() {
+  if (posts.length === 0) {
+    const emptyHTLM = `<div class="posts__no-cards"><div class="posts__no-cards__card"></div><h2>No post</h2><div>You no have a post, please create the first:)</div><button id="createPost" class="btn btn--secondary">Create a post +</button></div>`;
+    postsContainerHTML.innerHTML = emptyHTLM;
+    const createPost = document.getElementById("createPost");
+    createPost.addEventListener("click", openModal);
+    return;
+  }
+  postsContainerHTML.classList.add("posts");
+
+  const postsHTML = posts
+    .map(
+      (post) => `
+      <div class="card">
+        <button class="card__close modal__close" data-id="${post.id}>
+          <i class="fa-sharp fa-solid fa-xmark"></i>
+        </button>
+        <img src="${post.img}"/>
+        <div class="card__info">
+          <h2>${post.title}</h2>
+          <p>${post.description}</p>
+        </div>
+      </div>`
+    )
+    .reverse()
+    .join(" ");
+  postsContainerHTML.innerHTML = postsHTML;
+}
